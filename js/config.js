a12994441b355ee3eaef2e6265513a295e80ff93
Joao Paulo Furtado <joaopaulofurtado@live.com>
2019-08-21 18:04:13 -0700

    finished

--- js/config.js
+++ js/config.js
@@ -1,44 +1,35 @@
 const config = {
-
   // mode
-  'mode': 'dev', //please set to prod before pushing to github pages!
+  mode: "prod", //please set to prod before pushing to github pages!
   //prod mode will turn on tracking, so AVOID visiting your own page, since it will lower your conversion rate
   //I only set it to dev while adjusting variables here to avoid tracking my own page visits.
 
-
   //Tracking
 
-  'ideaname': 'workwards',
-
+  ideaname: "workwards_tempstaff",
 
   // Content
 
-  'headline': 'Job Hopping made easy',
-  'subtitle': 'Receive better job offers while employed',
-  'first-mobile-mockup':'images/workwards-main.png',
-
-
-
+  headline: "Earn money by working in your free time",
+  subtitle: "On-demand staff app for trusted companies in BC",
+  "first-mobile-mockup": "images/workwards-main.png",
 
   //Benefits
 
-  'second-mobile-mockup': 'images/workwards-offers.png',
-  'second-mobile-title': 'Receive Offers',
-  'second-mobile-description': 'Just create your anonymous profile and start receiving offers! You can also set a minimum bid, maximizing your chances to find a better deal.',
+  "second-mobile-mockup": "images/workwards-offers.png",
+  "second-mobile-title": "Receive Offers",
+  "second-mobile-description":
+    "Whenever there's a big event near you, staff on demand is needed. Receive a notification to apply as soon as its posted in our platform.",
 
+  "third-mobile-mockup": "images/workwards-recruiters.png",
+  "third-mobile-title": "Chat with Recruiters",
+  "third-mobile-description":
+    "After accepting an offer you can instantly start chatting with potential hirers and schedule an interview",
 
-  'third-mobile-mockup': 'images/workwards-recruiters.png',
-  'third-mobile-title': 'Chat with Recruiters',
-  'third-mobile-description': 'After accepting an offer you can instantly start chatting with potential employers and schedule an interview',
+  "fourth-mobile-mockup": "images/iphone-feature-01.png",
+  "fourth-mobile-title": "fourth mobile",
+  "fourth-mobile-description": "fourth mobile description",
 
-
-  'fourth-mobile-mockup': 'images/iphone-feature-01.png',
-  'fourth-mobile-title': 'fourth mobile',
-  'fourth-mobile-description': 'fourth mobile description',
-
-
-
-
-  'footer-headline': 'Job Hopping made easy',
-  'footer-subtitle': 'Receive better job offers while employed'
+  "footer-headline": "Staff-on-demand for BC",
+  "footer-subtitle": "Increase your income during your spare time"
 };
